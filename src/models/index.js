5839ad327d216bf7ff4134e76bb387a6307f031e
Nate Brown <nathan.d.brown@gmail.com>
2023-04-13 14:56:37 -0700

    conflicts

--- src/models/index.js
+++ src/models/index.js
@@ -1,18 +1,18 @@
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
-const userModel = require('./clothes/user.js');
-const pokemonModel = require('./food/pokemon.js');
-const Collection = require('./data-collection.js');
+const userModel = require('./user.js');
+const pokemonModel = require('./pokemon.js');
+const Collection = require('./collection.js');
 
-const DATABASE_URL = process.env.DATABASE_URL || 'sqlite:memory:';
+const DATABASE_URL = 'sqlite:memory:';
 
 const sequelize = new Sequelize(DATABASE_URL);
 const pokemon = pokemonModel(sequelize, DataTypes);
 const user = userModel(sequelize, DataTypes);
 
 module.exports = {
-  db: sequelize,
+  sequelize: sequelize,
   user: new Collection(user),
-  pokemon: new Collection(pokemon),
+  pokemon: new Collection(pokemon)
 };
